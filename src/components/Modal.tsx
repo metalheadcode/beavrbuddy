91e60e1e6b864fe8337034c4832a20693a541fd5
Ahmad Hazim Ahmad Fuad <ahmad.hazim@devwiz.my>
2024-05-30 13:23:15 +0800

    update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Modal({ children, isOpen }: Props) {
   if (isOpen) {
     return (
-      <div className="z-50 fixed inset-0 bg-black/50 backdrop-blur-xl flex items-center justify-center">
+      <div className="z-50 fixed inset-0 bg-black/50 backdrop-blur-xl flex items-center justify-center overflow-y-scroll no-scrollbar">
         {children}
       </div>
     );
